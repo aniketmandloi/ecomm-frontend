d2a17b0f77ddc2accc875d5c789553b17e20bd7b
Aniket Mandloi <aniketmandloi@Anikets-MacBook-Air.local>
2024-11-06 23:09:56 +0530

    add header nav

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import Register from "./routes/Register/Register";
 import Login from "./routes/Login/Login";
 import Home from "./routes/Home/Home";
 import ProductDetails from "./routes/ProductDetails/ProductDetails";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { useAppSelector } from "../../store/hooks";
+import {
+  AppBar,
+  Badge,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+import { SxProps, Theme } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const Header: React.FC = () => {
+  const isAuthenticated = useAppSelector((state) => state.auth);
+  const { items } = useAppSelector((state) => state.cart);
+
+  // Define styles using `sx` prop
+  const appBarSx: SxProps<Theme> = {
+    position: "static",
+    bgcolor: "primary.main",
+    boxShadow: 1,
+  };
+
+  const toolbarSx: SxProps<Theme> = {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  };
+
+  const titleSx: SxProps<Theme> = {
+    flexGrow: 1,
+    fontWeight: 600,
+  };
+
+  return (
+    <AppBar position="static" sx={appBarSx}>
+      <Toolbar sx={toolbarSx}>
+        <Typography variant="h6" sx={titleSx}>
+          Codecademy Shop
+        </Typography>
+        <div>
+          {!isAuthenticated && (
+            <Button color="inherit" component={Link} to={`/login`}>
+              Login
+            </Button>
+          )}
+          {isAuthenticated && (
+            <Button color="inherit" component={Link} to={`/orders`}>
+              My Orders
+            </Button>
+          )}
+          <IconButton
+            aria-label="access shopping cart"
+            color="inherit"
+            component={Link}
+            to="/cart"
+          >
+            <Badge badgeContent={items?.length || 0} color="secondary">
+              <ShoppingCart />
+            </Badge>
+          </IconButton>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
